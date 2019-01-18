b0ac46e5655243bb83156226b73a5ab7b081e400
My US No <shalini@andronivans-macbook-pro-2.local>
2019-01-17 22:02:49 -0500

    ToDoList complete with datasource and delegate methods.

---
+++ TodoList/ToDoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Shalini on 1/17/19.
+//  Copyright © 2019 Shalini. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+	let itemArray = ["Find Mike", "Buy Eggs", "Destory Demo"]
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray.count
+	}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+		cell.textLabel?.text = itemArray[indexPath.row]
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//print(itemArray[indexPath.row])
+	
+		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .none
+		} else {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+		}
+		
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
+}
+
